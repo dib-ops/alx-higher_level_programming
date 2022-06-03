7cc70d9c44055384bd8803e7488e9dbcb3eb3968
dib-ops <fayemathias0@gmail.com>
2022-06-03 02:46:05 +0000

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    num = len(sys.argv)
+    if num == 1:
+        print(f"{num - 1} arguments.")
+    elif num == 2:
+        print(f"{num - 1} argument:")
+    else:
+        print(f"{num - 1} arguments:")
+
+    for i in range(1, num):
+        print(f"{i}: {sys.argv[i]}")
